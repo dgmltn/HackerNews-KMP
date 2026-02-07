03fe0f10dbe52312a52ce9b5484f2430c38b8969
Doug Melton <what@supbro.com>
2026-02-06 23:48:13 -0800

    Fixed second-run crash
    After launching the app a second time (after a few minutes) it would
    often crash. This ensures only one DataStore is created in the lifecycle
    of the process, not just across the App object lifecycle.
    
    > java.lang.IllegalStateException: There are multiple DataStores active
    > for the same file:
    > /data/data/com.jarvislin.hackernews/files/hn.preferences_pb. You should
    > either maintain your DataStore as a singleton or confirm that there is
    > no two DataStore's active on the same file (by confirming that the scope
    > is cancelled).

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,26 +1,12 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import modules.dataModule
-import modules.repositoryModule
-import modules.useCaseModule
-import modules.viewModelModule
-import org.koin.compose.KoinApplication
 import presentation.RootScreen
 import ui.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    KoinApplication(application = {
-        modules(
-            dataModule,
-            repositoryModule,
-            useCaseModule,
-            viewModelModule
-        )
-    }) {
-        AppTheme {
-            RootScreen()
-        }
+    AppTheme {
+        RootScreen()
     }
 }
